6781ca13abd7f265da431ded4ecd71059d9614e2
Oliver Pearce <OliverPearce13@gmail.com>
2023-04-11 01:18:47 -0700

    added slash commands, restructured project

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// Require the necessary discord.js classes and dotenv (for token)
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+const fs = require('node:fs'); 
+const path = require('node:path');
 
-import { Client, GatewayIntentBits } from 'discord.js';
-
+// Create a new client instance
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -12,13 +14,51 @@ const client = new Client({
     ],
 });
 
-client.on('messageCreate', async (message) => {
-    console.log(message);
+// create a collection for commands, easy to store and retrieve for execution!
+client.commands = new Collection();
 
-    if (!message?.author.bot) {
-        message.author.send(`Echo ${message.content}`);
-    }
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// If bot is able to connect, print a confirmation message!
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag} :D`);
 });
 
-client.login(process.env.DISCORD_TOKEN);
+// execute (slash) commands!
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+// Log in to Discord with your client's token
+client.login(token);
